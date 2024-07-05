0bec73629ef0b31c94018c6e351b4a2c7997c432
Lukas <75340352+Kory291@users.noreply.github.com>
2024-07-05 12:21:02 +0000

    erste Version vom Rechner

--- calculator.py
+++ calculator.py
@@ -1,8 +1,39 @@
+i = 0
+GesamtNote = 0
+
+def NeueNote():
+    global GesamtNote
+    global i
+    print("Gib bitte eine Note an, welche du speichern möchtest:")
+    Note = input()
+    if Note:
+        Note = int(Note)
+        if 0<Note<7:
+            Noten.append(Note)
+            print(Noten)
+        else:
+            print("Please input a valid grade.")
+    else:
+        print("Please input a valid grade.")
+    GesamtNote += Note
+    i += 1
+    Finish()
+
+def Finish():
+    Finished = input("Are you finished? [y/n]")
+    print(Finished)
+    if Finished == "y":
+        print(GesamtNote)
+        Durchschnitt = GesamtNote/len(Noten)
+        print(Durchschnitt)
+    elif Finished == "n":
+        NeueNote()
+    else:
+        print(Finished)
+        print("Please give a valid input")
+        Finish()
+
 if __name__ == '__main__':
     print("Willkommen zum Notenrechner!")
-    while True:
-        print("Gib bitte das Fach an, für welches du die Note speichern möchtest:")
-        fach = input()
-        
-        
-
+    Noten = []
+    NeueNote()
